385c87d5f140c483cc02a5a1a7ffd7fb6d42da80
whpz <2206373437@qq.om>
2024-12-21 16:43:06 +0800

    验证码登录和注册相关代码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.hmdp.service.impl;
 
 import cn.hutool.Hutool;
 import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.util.Random;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -36,6 +40,34 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         session.setAttribute("code", code);
         //发送验证码
         log.debug("发送的验证码是：" + code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //先校验手机号
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号格式错误");
+        }
+        //再获取session中的验证码
+        Object code = session.getAttribute("code");
+        //与传来的验证码比较
+        String userCode = loginForm.getCode();
+        //若不一致 则返回错误
+        if (code == null || !userCode.equals(code.toString())) {
+            return Result.fail("验证码错误");
+        }
+        //若一致 则根据电话查询用户
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        //若用户不存在 则注册该用户
+        if (user == null) {
+            user = new User();
+            user.setPhone(loginForm.getPhone());
+            user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(10));
+            save(user);
+        }
+        //将用户存入session中
+        session.setAttribute("user",user);
         return Result.ok();
     }
 }
