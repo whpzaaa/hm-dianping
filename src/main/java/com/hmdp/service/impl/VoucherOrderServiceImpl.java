09c9648cd682e8f27f68e61a043438773f0a8a6f
whpz <2206373437@qq.om>
2025-01-04 18:04:22 +0800

    优惠券秒杀

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +25,47 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
-
+    @Autowired
+    private ISeckillVoucherService seckillVoucherService;
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        //查询优惠券
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        //判断是否在有效时间内
+        LocalDateTime now = LocalDateTime.now();
+        if (seckillVoucher.getBeginTime().isAfter(now)){
+            return Result.fail("秒杀尚未开始");
+        }
+        if (seckillVoucher.getEndTime().isBefore(now)){
+            return Result.fail("秒杀已经结束");
+        }
+        //判断库存是否充足
+        if (seckillVoucher.getStock() < 1 ) {
+            return Result.fail("库存不足");
+        }
+        //充足 库存减一
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                //为防止多线程并发问题 应设置乐观锁 即stock应该与之前查到的一致 但是这样会
+                //造成不必要的失败 所以当库存大于0时都可以执行减一
+                .gt("stock", 0).update();
+        if (!success) {
+            return Result.fail("库存不足");
+        }
+        //创建订单
+        //生成id
+        long id = redisIdWorker.nextId("order");
+        VoucherOrder voucherOrder = new VoucherOrder();
+        voucherOrder.setId(id);
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        //返回订单编号
+        return Result.ok(id);
+    }
 }
+
